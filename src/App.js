39c0d25c0f5558859dafce86a5db248b00ea2b29
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-05 20:23:58 +0300

    Вывожу спинер при загрузке

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import axios from "axios";
 import spinner from './img/Spin-1s-200px.svg'
+import {useState} from "react";
 
 function App() {
+    let [load, setLoad] =  useState(true)
 
-    let load = true;
     console.log(load)
     axios.get("http://jsonplaceholder.typicode.com/posts")
         .then(res => {
             console.log(res.data);
-            load = false;
+            setLoad(false);
         }, reason => {
             console.log(reason)
         })
