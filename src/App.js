7a65e67c19a632e9efcde779b653cedbf1e345e1
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-05 21:56:00 +0300

    Начинаю мапинг

--- src/App.js
+++ src/App.js
@@ -5,34 +5,42 @@ import {useState} from "react";
 import axios from "axios";
 
 function App() {
-    let users = {
-        userId:null,
-        id: null,
-        title:null,
-        body:null
+    let users = []
+    // let user = {
+    //     userId: null,
+    //     id: null,
+    //     title: null,
+    //     body: null
+    // }
+    const setUsers = (response)=> {
+        users.push(response)
     }
-    let [load, setLoad] = useState(true)
 
+    let [load, setLoad] = useState(true)
 
     const getFunction = () => {
         axios.get("http://jsonplaceholder.typicode.com/posts")
             .then(res => {
-                console.log(res.data);
+                setUsers (res.data);
                 setLoad(false);
-            }, reason => {
-                console.log(reason)
             })
+            .catch(error => {
+                console.log("error", error);
+            });
     }
 
     console.log('Статус загрузки:' + load)
-    getFunction()
+     getFunction()
     console.log('Статус загрузки:' + load)
-
+    console.log(users)
 
     return (
         <div>
             {load && <img src={spinner} alt="Спинер"/>}
+            {users.map(u =>
+            <div>u.userId</div>
 
+            )}
 
         </div>
     );
