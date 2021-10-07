3375aa2709ffbff82058d0604716d0089c338596
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-07 11:07:25 +0300

    Добавил поле ввода с реакцией

--- src/App.js
+++ src/App.js
@@ -5,36 +5,55 @@ import axios from "axios";
 
 function App() {
 
-    const [posts, setPosts] = useState([
-    ])
-    const [loading,setLoading] =useState(false)
+    const [posts, setPosts] = useState([])
+    const [loading, setLoading] = useState(false)
+
 
     useEffect(() => {
-        setLoading(true)
-        console.log("Начало загрузки"+loading)
+            setLoading(true);
+
             axios.get("http://jsonplaceholder.typicode.com/posts")
                 .then(response => {
-                    setLoading(false)
-                    console.log("Конец загрузки" + loading)
-                    setPosts(response.data)})
+                    setLoading(false);
+                    setPosts(response.data)
+                })
                 .catch(error => console.log(error))
 
         }, []
     );
 
 
+    const MessageFilter = (searchText) => {
+        console.log (searchText.currentTarget.value)
+    }
+
+
+
     return (
-        <div>
+        <div className="messageTableBlock">
+
             {loading && "Loading"}
+<div className="searchBlock">
+    Search:
+
+    <input type="text"  id="searchField" key="searchField" onChange={MessageFilter} />
+
+</div>
 
             <table>
                 <tbody>
-                {posts.map((post,index) =>
+                <th>№ п/п</th>
+                <th>user Id</th>
+                <th>post Id</th>
+                <th>message body</th>
+                {posts.map((post, index) =>
 
                     <tr key={index}>
+
                         <td> {index}</td>
+                        <td> {post.userId}</td>
                         <td> {post.id}</td>
-                        <td>{post.title}</td>
+                        <td id="messageBody">{post.title}</td>
                     </tr>)}
                 </tbody>
             </table>
