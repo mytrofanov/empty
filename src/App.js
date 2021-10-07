863681996416057348b33ecf975893d8b53d2f8b
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-07 15:04:09 +0300

    Фильтруется поле title

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import axios from "axios";
 
 function App() {
 
-    const [posts, setPosts] = useState([])
+    const [postsFromServer, setPostsFromServer] = useState([])
+    const [filteredPosts, setFilteredPosts] = useState([])
     const [loading, setLoading] = useState(false)
+    const [q, setQ] = useState("")
+
 
 
     useEffect(() => {
@@ -15,38 +18,60 @@ function App() {
             axios.get("http://jsonplaceholder.typicode.com/posts")
                 .then(response => {
                     setLoading(false);
-                    setPosts(response.data)
+                    setPostsFromServer(response.data)
+                    setFilteredPosts(response.data)
                 })
                 .catch(error => console.log(error))
 
         }, []
     );
+    const filtered = (e) => {
+      const filtered =
+          postsFromServer &&
+          postsFromServer.filter((item) => {
+              return item.title.toLowerCase().includes(e)
+          })
+          setFilteredPosts(filtered)
+
+      }
 
 
-    const MessageFilter = (searchText) => {
-        console.log (searchText.currentTarget.value)
-    }
+    // const MessageFilter = (searchText) => {
+    //      let searchWord = searchText.currentTarget.value
+    //     resultPosts = posts.find(word => word.title.includes(searchWord))
+    //        }
 
-
+console.log (filteredPosts)
 
     return (
         <div className="messageTableBlock">
 
             {loading && "Loading"}
-<div className="searchBlock">
-    Search:
+            <div className="searchBlock">
+                Search:
 
-    <input type="text"  id="searchField" key="searchField" onChange={MessageFilter} />
+                <input type="text" id="searchField"
+                       placeholder="search"
+                       value={q}
+                       key="searchField"
+                       onChange={(event => {
+                           setQ(event.target.value)
+                           filtered(event.target.value)
+                       })}/>
 
-</div>
+            </div>
 
             <table>
                 <tbody>
                 <th>№ п/п</th>
                 <th>user Id</th>
                 <th>post Id</th>
+                <th>message title</th>
                 <th>message body</th>
-                {posts.map((post, index) =>
+
+                {filteredPosts && filteredPosts.length >0 ?
+                    filteredPosts.map((post, index) =>
+
 
                     <tr key={index}>
 
@@ -54,7 +79,10 @@ function App() {
                         <td> {post.userId}</td>
                         <td> {post.id}</td>
                         <td id="messageBody">{post.title}</td>
-                    </tr>)}
+                        <td id="messageBody">{post.body}</td>
+                    </tr>): null
+                }
+
                 </tbody>
             </table>
         </div>
